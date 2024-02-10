6835b5936943a8deba188a8bafb05cfb7f863bce
unknown <byadiso@gmail.com>
2024-02-10 15:18:11 +0100

    implementing redict on sign in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import "../Style/login_register.css";
-import { Link} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { isAuthenticated, login } from "../firebase/Authentication";
 import { checkMyValue } from "../firebase/Helpers";
 
@@ -11,12 +11,13 @@ function Login() {
   const [error, setError] = useState();
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+
   // const [userId, setUserId] = useState();
 
+  console.log(isLoggedIn)
 
-
-
-  
+  const navigate = useNavigate();
 
 
  
@@ -37,18 +38,27 @@ function Login() {
     event.preventDefault();    
     checkMyValue(user, setError);   
     login(user.email, user.password);
+    setSuccessMessage("Logged in successfully")
+    navigate("/Login");
   };
 
   useEffect(() => {
     isAuthenticated(setIsLoggedIn);
-  }, []);
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/Login");
+    }
+  }, [navigate, isLoggedIn]);
 
   return (
     <div>
       <Navbar />
       <section>
-        {isLoggedIn? <Link to="/"/> : <div className="service_section">         
+       
+        {!isLoggedIn && <div className="service_section">         
           <div className="service_item_left login_form" id="login_form">
+          {successMessage && <p>{successMessage}</p>}
             <div className="service_form_item form_login">      
               <h3>Login to LearnByWriting.com</h3>
               <input
@@ -89,6 +99,7 @@ function Login() {
             </div>
           </div>
         </div>}
+        
       </section>
       <Footer />
     </div>
--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,41 +2,44 @@ import React, { useEffect, useState } from "react";
 import "../Style/user.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LogoutUser, isAuthenticated } from "../firebase/Authentication";
 
 function Logout() {
+  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [userId, setUserId] = useState();
-console.log(isLoggedIn)
-  useEffect(() => {
-    isAuthenticated(setIsLoggedIn);
-  }, []);
+
+  //   console.log(isLoggedIn);
 
   const HandleLogOut = () => {
     LogoutUser();
   };
 
+  useEffect(() => {
+    isAuthenticated(setIsLoggedIn);
+    if (!isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/Logout");
+    }
+  }, [navigate, isLoggedIn]);
+
   return (
     <div>
       <Navbar />
-      {!isLoggedIn ? (
-        <h2>Not logged in</h2>
-      
-      ) : (
-        <div>
-          {" "}
-          <h2>Are you sure you want to log out ?</h2>
-          <div className="logout_button">
-            <button id="logout" onClick={HandleLogOut}>
-              Yes
-            </button>
-            <button id="return">
-              <Link to="/">Return Home</Link>
-            </button>
-          </div>
+
+      <div>
+        {" "}
+        <h2>Are you sure you want to log out ?</h2>
+        <div className="logout_button">
+          <button id="logout" onClick={HandleLogOut}>
+            Yes
+          </button>
+          <button id="return">
+            <Link to="/">Return Home</Link>
+          </button>
         </div>
-      )}
+      </div>
 
       <Footer />
     </div>
