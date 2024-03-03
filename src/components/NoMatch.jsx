45e80173c462b99929743a08211590eab428c830
unknown <byadiso@gmail.com>
2024-03-03 07:51:02 +0100

    modify no match page with better UX

--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import "../Style/Style.css";
+import { Link } from 'react-router-dom';
 
 function NoMatch() {
   return (
     <div>
       <Navbar />
       
-      <div className='not_found'>
-      <p > 404 No Match found!</p>
+      <div className='not_found' style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
+      <p style={{color: "black"}}> 404 No Match found!</p>
+      <Link to="/"><p style={{color: "black"}}>Return to Home Page</p></Link>
       </div>     
 
       <Footer />
