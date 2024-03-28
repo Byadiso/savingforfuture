aabdb53177fb1d8ca6951087da851a038745d0f2
unknown <byadiso@gmail.com>
2024-03-28 11:54:15 +0100

    fix styling on user page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { getLoggedUser, isAuthenticated} from '../firebase/Authentication';
-import NoAccess from './NoAccess';
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { getLoggedUser, isAuthenticated } from "../firebase/Authentication";
+import NoAccess from "./NoAccess";
 
 function UserProfile() {
   const [loggedUser, setLoggedUser] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
   let email = loggedUser.email;
   let userId = loggedUser.uid;
 
-  useEffect(() => {    
+  useEffect(() => {
     getLoggedUser(setLoggedUser);
     isAuthenticated(setIsLoggedIn);
-  }, [ isLoggedIn]);
-
+  }, [isLoggedIn]);
 
   return (
-    <div >
+    <div>
       <Navbar />
-      {isLoggedIn ? 
-      <div>
-      <h1>UserProfile</h1>
-      <p >Email: {email && email}</p>
-      <p >Uid: {userId && userId}</p>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {isLoggedIn ? (
+          <div>
+            <h1>UserProfile</h1>
+            <p>Email: {email && email}</p>
+            <p>Uid: {userId && userId}</p>
+          </div>
+        ) : (
+          <NoAccess />
+        )}
       </div>
-      :  <NoAccess />}
       <Footer />
-      </div>
-  )
+    </div>
+  );
 }
 
-export default UserProfile
+export default UserProfile;
