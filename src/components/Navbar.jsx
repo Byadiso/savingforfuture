5ead8d41553a16fa0da79c1a53187464822b2823
unknown <byadiso@gmail.com>
2024-04-01 14:40:54 +0200

    record card on dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,13 @@ import React, { useEffect, useState } from "react";
 import "../Style/NavBar.css";
 import { Link, useLocation } from "react-router-dom";
 import { isAuthenticated } from "../firebase/Authentication";
-import SignOutDiaolog from "./SignOut";
-import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
-import AutoStoriesIcon from "@mui/icons-material/AutoStories";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import PersonIcon from "@mui/icons-material/Person";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import LoginIcon from "@mui/icons-material/Login";
-import LightModeIcon from "@mui/icons-material/LightMode";
+import PaymentsIcon from "@mui/icons-material/Payments";
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const location = useLocation();  
+  const location = useLocation();
 
   const style = {
     display: "flex",
@@ -32,25 +26,16 @@ function Navbar() {
         <div className="menu">
           <h2 className="ngana_logo">
             <Link className="menu_nav" to="/" style={style}>
-              <LightModeIcon fontSize="large" style={{ color: "white" }} />
+              <PaymentsIcon fontSize="large" style={{ color: "white" }} />
               Bugdeto
             </Link>
-          </h2> 
+          </h2>
           <Link to="/Dashboard" style={style}>
-                <DashboardIcon className="Dashboard_dropdown"/> <span className={ location.pathname ==="/Dashboard"? "": null}>Dashboard </span>
-          </Link>   
-          <Link
-                  className=" dropbtn"
-                  to="/User"
-                  id="username"
-                  style={{ color: "white" }}
-                >
-                  <PersonIcon />
-                  User
-                </Link>   
-          
-          
-         
+            <DashboardIcon className="Dashboard_dropdown" />{" "}
+            <span className={location.pathname === "/Dashboard" ? "" : null}>
+              Dashboard{" "}
+            </span>
+          </Link>
         </div>
       </nav>
     </div>
